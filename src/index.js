7a680a033ab6222a706a6424aeb345799901a397
taoxiaofeng <taoxiaofeng@mchz.com.cn>
2021-12-17 09:35:29 +0800

    useRef

--- src/index.js
+++ src/index.js
@@ -3,12 +3,20 @@
  */
 import React from 'react';
 import ReactDOM from 'react-dom';
+import MyComponent from './components/FC/MyComponent.js';
 import Game from './components/Game/Game';
+import Count from './components/Count/Count';
 import './index.css';
+import UseRefDemo from './components/UseRef/useRef';
 
 // ========================================
 
+// import  MyComponent  from './components/FC/MyComponent';
+
+console.log(<Game />)
 ReactDOM.render(
-    <Game />,
+    // <Game />,
+    // <MyComponent />,
+    <UseRefDemo />,
     document.getElementById('root')
 );
