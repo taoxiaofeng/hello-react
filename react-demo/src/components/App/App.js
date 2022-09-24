176a60380581cbcee704e004d1c5b065aeac6213
taoxiaofeng <taoxiaofeng55@qq.com>
2022-09-24 17:13:49 +0800

    eject

--- react-demo/src/components/App/App.js
+++ react-demo/src/components/App/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import styles from './App.module.css'
+import Css from './App.module.css'
 import Hello from '../Hello';
 function App() {
   const len = 20;
-  return <ul className={styles.bg}>
+  return <ul className={Css.bg}>
     {Array(len).fill(0).map((_, i) => <li key={i}>{i}</li>)}
     <Hello />,
   </ul>
