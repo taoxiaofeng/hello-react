229b48799edcae0b1b9978acbc12d2d015a1e44f
taoxiaofeng <taoxiaofeng55@qq.com>
2022-10-04 23:37:39 +0800

    fetch

--- my-app/src/components/GitHubUser/Search/index.tsx
+++ my-app/src/components/GitHubUser/Search/index.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import axios from "axios";
+// import axios from "axios";
 import styles from "./index.module.less";
 import { Button, Input } from "antd";
 import PubSub from "pubsub-js";
@@ -9,23 +9,36 @@ interface Props {}
 function Search(props: Props) {
   const [value, setValue] = useState("");
 
-  const search = () => {
+  const search = async () => {
     //发送请求前通知List更新状态
-    PubSub.publish("updateInfo", { isFirst: false, isLoading: true });
-    //发送网络请求
-    axios.get(`/api1/search/users?q=${value}`).then(
-      (response) => {
-        //请求成功后通知List更新状态
-        PubSub.publish("updateInfo", {
-          isLoading: false,
-          users: response.data.items,
-        });
-      },
-      (error) => {
-        //请求失败后通知App更新状态
-        PubSub.publish("updateInfo", { isLoading: false, err: error.message });
-      }
-    );
+    // PubSub.publish("updateInfo", { isFirst: false, isLoading: true });
+    // //发送网络请求
+    // axios.get(`/api1/search/users?q=${value}`).then(
+    //   (response) => {
+    //     //请求成功后通知List更新状态
+    //     PubSub.publish("updateInfo", {
+    //       isLoading: false,
+    //       users: response.data.items,
+    //     });
+    //   },
+    //   (error) => {
+    //     //请求失败后通知App更新状态
+    //     PubSub.publish("updateInfo", { isLoading: false, err: error.message });
+    //   }
+    // );
+
+    // 发送网络请求 --- 使用fetch发送
+    try {
+      const res = await fetch(`/api1/search/users?q=${value}`);
+      const data = await res.json();
+      console.log(`data -- `, data);
+      PubSub.publish("updateInfo", {
+        isLoading: false,
+        users: data.items,
+      });
+    } catch (error) {
+      PubSub.publish("updateInfo", { isLoading: false, err: error.message });
+    }
   };
 
   return (
