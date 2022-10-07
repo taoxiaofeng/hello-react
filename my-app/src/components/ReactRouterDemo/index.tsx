611fd5f58ca9be7aade70aef3d1663c465f7ff8e
taoxiaofeng <taoxiaofeng55@qq.com>
2022-10-07 20:49:41 +0800

    router exact

--- my-app/src/components/ReactRouterDemo/index.tsx
+++ my-app/src/components/ReactRouterDemo/index.tsx
@@ -1,9 +1,9 @@
 import React from "react";
-import { NavLink, Route, Switch } from "react-router-dom";
+import { NavLink, Route, Switch, Redirect } from "react-router-dom";
 import styles from "./index.module.less";
 import Home from "./Home";
 import About from "./About";
-import Test from './Test';
+// import Test from './Test';
 
 interface Props { }
 
@@ -15,15 +15,15 @@ function ReactRouterDemo(props: Props) {
       <section>
         <div className={styles.left}>
           {/* 编写路由链接 */}
-          <NavLink className='nav-link' to={`/base/about`}>About</NavLink>
-          <NavLink className='nav-link' to={`/base/home`}>Home</NavLink>
+          <NavLink className='nav-link' to={`/about`}>About</NavLink>
+          <NavLink className='nav-link' to={`/home/a/b`}>Home</NavLink>
         </div>
         <div className={styles.right}>
           {/* 注册路由  */}
           <Switch>
-            <Route path="/base/about" component={About} />
-            <Route path="/base/home" component={Home} />
-            <Route path="/base/home" component={Test} />
+            <Route exact={true} path="/about" component={About} />
+            <Route exact={false} path="/home" component={Home} />
+            <Redirect to='/about'/>
           </Switch>
         </div>
       </section>
